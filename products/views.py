59ff852b1c149b8f8a84fe7c0e9bee2a68504c2e
yoonus <yoonusuns69@gmail.com>
2024-03-14 21:02:20 +0530

    data model

--- products/views.py
+++ products/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from . models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def list_products(request):
-
-    return render(request,'products.html')
+    page = 1
+    if request.GET:
+        page = request.GET.get('page',1)
+    product_list = Product.objects.all()
+    product_paginator = Paginator(product_list,2)
+    product_list = product_paginator.get_page(page)
+    context = {'products': product_list}
+    return render(request,'products.html',context)
 def detail_product(request):
     return render(request,'product_details.html')
