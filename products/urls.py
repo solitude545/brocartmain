3944b469081ee46f17d63e143616ccf08cb103ab
yoonus <yoonusuns69@gmail.com>
2024-03-19 20:54:40 +0530

    user account frst

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.index,name='home'),
     path('product_list',views.list_products,name='list_product'),
-    path('product_details',views.detail_product,name='detail_product')
+    path('product_details/<pk>',views.detail_product,name='detail_product')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
