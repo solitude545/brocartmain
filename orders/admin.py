9ef2fbfcc47fdc3c722143f8a7f8a75f8586a6da
yoonus <yoonusuns69@gmail.com>
2024-04-06 20:43:21 +0530

    cart final stage

--- orders/admin.py
+++ orders/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Order,OrderedItem
-# Register your models here.
-admin.site.register(Order)
+# Register your models here
+class OrderAdmin(admin.ModelAdmin):
+    list_filter = [
+        "owner",
+        "order_status",
+    ]
+    search_fields = [
+        "owner",
+        "id",
+    ]
+admin.site.register(Order,OrderAdmin)
